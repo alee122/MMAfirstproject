36d943b91025be67ebe38a76ef89c7c293d950f6
Sophie <sophiedasinger@wr-130-64-194-168.medford.tufts.edu>
2016-02-09 17:55:35 -0500

    added sample flashlight code

--- PedestrianSafetyApp/ViewController.swift
+++ PedestrianSafetyApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -24,20 +25,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func flashlightButton(sender: UIButton) {
-
         if (lightIsOn) {
             print("turning off flash!")            
             //turn off light
+            toggleFlash()
             sender.setTitle("TURN ON FLASHLIGHT", forState: .Normal)
             lightIsOn = !lightIsOn
         } else {
             print("turning on flash!")
             //turn on light
+            toggleFlash()
             sender.setTitle("TURN OFF FLASHLIGHT", forState: .Normal)
             lightIsOn = !lightIsOn
         }
         
     }
+    
+    func toggleFlash() {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureTorchMode.On) {
+                    device.torchMode = AVCaptureTorchMode.Off
+                } else {
+                    do {
+                        try device.setTorchModeOnWithLevel(1.0)
+                    } catch {
+                        print(error)
+                    }
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
 
 }
-
