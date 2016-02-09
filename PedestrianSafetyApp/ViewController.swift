46fac95aee7aa1dafbf399e2ffe5acc335753aa4
rachelfrenkel <rach.frenkel@gmail.com>
2016-02-09 17:17:53 -0500

    added flashlight button

--- PedestrianSafetyApp/ViewController.swift
+++ PedestrianSafetyApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flashlightButton: UIButton!
+    var lightIsOn = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func flashlightButton(sender: UIButton) {
+
+        if (lightIsOn) {
+            print("turning off flash!")            
+            //turn off light
+            sender.setTitle("TURN ON FLASHLIGHT", forState: .Normal)
+            lightIsOn = !lightIsOn
+        } else {
+            print("turning on flash!")
+            //turn on light
+            sender.setTitle("TURN OFF FLASHLIGHT", forState: .Normal)
+            lightIsOn = !lightIsOn
+        }
+        
+    }
 
 }
 
